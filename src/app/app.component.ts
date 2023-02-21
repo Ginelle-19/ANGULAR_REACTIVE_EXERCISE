f4f3d422c9f5d30e3a675129c4da09a0541447be
Ginelle-19 <101726053+Ginelle-19@users.noreply.github.com>
2023-02-21 13:14:57 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { of, Observable, from } from 'rxjs';
+import { of, Observable, from, filter } from 'rxjs';
 import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
@@ -11,25 +11,25 @@ export class AppComponent {
   numbers$: Observable<number>;
   constructor() {}
 }
+//create array
 const numbers$ = of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-
+//subscribing to the observable
 numbers$.subscribe((value) => console.log(value));
-
+//manipulate data using map
 numbers$
   .pipe(map((value) => value * 2))
   .subscribe((value) => console.log(value));
-
+//create an observable with an array of user options
 const users = [
   { id: 1, name: 'Alice', age: 25 },
   { id: 2, name: 'Bob', age: 30 },
   { id: 3, name: 'Charlie', age: 35 },
   { id: 4, name: 'Dave', age: 40 },
 ];
-
+//using from operator
 const users$ = from(users);
-
 users$.subscribe((users) => console.log(users));
-
+//use rxjs to multiply age and display ALL CAPS
 users$
   .pipe(
     map((users) => ({
@@ -39,3 +39,18 @@ users$
     }))
   )
   .subscribe((users) => console.log(users));
+//LAB 10 EXERCISE
+console.log('\n\nEven:');
+numbers$
+  .pipe(filter((numbers) => numbers % 2 == 0))
+  .subscribe((numbers) => console.log(numbers));
+
+console.log('\n\nOdd:');
+numbers$
+  .pipe(filter((numbers) => numbers % 2 != 0))
+  .subscribe((numbers) => console.log(numbers));
+
+console.log('\n\nOdd * 2:');
+numbers$
+  .pipe(filter((numbers) => numbers % 2 == 0))
+  .subscribe((numbers) => console.log(`${numbers} = ${numbers * 2}`));
